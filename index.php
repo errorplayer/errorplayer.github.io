a365e7072bd783d435ad216559870d99939f3033
Zeyang <linzeyang1997@yeah.net>
2018-10-14 17:05:15 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php
-	echo "你好，本页已经不再维护，正在为您跳转新链接......";
-	header(Refresh:5;url='http://47.100.222.211/en-learning.php');
-?>
+<html>
+	<meta http-equiv=”refresh” content=”3;url=’http://47.100.222.211/en-learning.php’” >
+	<h1>本页面已经不再维护，正在为您跳转新链接......</h1>
+</html>
